d4957efe1d259884ce9fb71043bbf282feab0980
tihonov-e <tom-step@ya.ru>
2020-04-29 08:57:31 +0300

    решена первая задача про функции

--- functions/functions/functions.cpp
+++ functions/functions/functions.cpp
@@ -9,20 +9,64 @@
 
 using namespace std;//
 
-#include <iostream>
+//void rec() {
+//    int n;
+//    cin >> n;
+//    if (n != 0) {
+//        rec();
+//        cout << n;
+//    }
+//}
+//
+//int fact(int n) {
+//    if (n == 0) {
+//        return 1;
+//    }
+//    return n * fact(n - 1);
+//}
+
+int min(int a, int b) {
+	int c = 0; //result
+	if (a < b) c = a;
+	else c = b;
+	return c;
+}
+
+int min4(int a, int b, int c, int d) {
+
+	int x = 0; //result
+
+	x = min(min(a, b), min(c, d));
+	return x;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//Напишите функцию mirn(a, b), вычисляющую минимум двух чисел.
+	//	Затем напишите функцию min4(a, b, c, d), вычисляющую минимум
+	//	4 чисел с помощью функции min.Считайте четыре целых числа и выведите их минимум.
+	//	Формат входных данных
+	//	Вводятся четыре целых числа.
+	//	Формат выходных данных
+	//	Выведите ответ на задачу.
+	//	Sample Input :
+
+	//	4
+	//	5
+	//	6
+	//	7
+	//	Sample Output :
+
+	//	4
+
+	int a = 0, b = 0, c = 0, d = 0; //вводимые числа
+
+	cin >> a; cin >> b; cin >> c; cin >> d;	//вводим числа с клавиатуры
+
+	cout << min4(a, b, c, d);
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

1cad81feb90eb24153c1f392f9bc6f09454b8ab8
tihonov-e <tom-step@ya.ru>
2021-04-09 23:22:51 +0300

    started learning map

--- SET_MAP/Source/map_practice.cpp
+++ SET_MAP/Source/map_practice.cpp
@@ -1,87 +1,29 @@
 ﻿
 //1.11 Словари и множества
 //Начало изучения
-
+//Словари
 
 #include <iostream>
 #include <string>
-#include <set>
+#include <map>
 
 using namespace std;
 
+map <int, string> s;
+
 int main()
 {	
-	multiset <int> s;
-	int n = 0; //число запросов
+	s[102] = "ments"; 
+	s[112] = "SOS";
 	
-	/**
-	1 - добавить элемент множества
-	2 - проверка наличия элемента в множестве
-	3 - удалить элемент
-	*/
-	int type = 1; //тип запроса
-	int x = 0; //число в запросе
-
-	cout << "input n = "; cin >> n;
-	cout << "input " << n << " members: ";
+	for (auto now : s) cout << now.first << " " << now.second << endl;
 	
-	for (int i = 0; i < n; i++) {
-		cin >> x;
-		s.insert(x);
-	}
-
-	//print set s
-	for (auto now : s) {
-		cout << now << " ";
-	}
-
-	cout << "\n";
+	int x = 0;
 	
-	cout << "command type: 1- add x; 2 - check x; 3 - delete x\n";  
-	cout << "4 - print all; 5 - check size; 6 - count x  0 - exit\n";
-	cout << "Enter command:\n";
+	cin >> x;
 
-	while (type != 0)
-	{		
-		cin >> type;
-
-		//insert element
-		if (type == 1) {
-			cin >> x;
-			s.insert(x); cout << x << " added";
-		}
-
-		//check element
-		else if (type == 2) {
-			cin >> x;
-			if (s.find(x) == s.end()) cout << "NO";
-			else cout << "YES";
-		}
-
-		else if (type == 3) {
-			cin >> x;
-			s.erase(x);  cout << x << " deleted";
-		}
-
-		else if (type == 4) {
-			for (auto now : s) {
-				cout << now << " ";
-			}
-		}
-
-		else if (type == 5) cout << "size = " << s.size();
-
-		else if (type == 6) {
-			cin >> x;
-			int cnt = 0;
-			for (auto now = s.lower_bound(x); now != s.upper_bound(x); now++) {
-				cnt++;
-			}
-			cout << " count" << "(" << x << ") =" << cnt;
-		}
-		
-		cout << "\n";
-	}
+	if (s.find(x) != s.end()) cout << "YES\n";
+	else cout << "NO\n";
 
 	return 0;
 }
